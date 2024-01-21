dfc6bb2f950c06850288735762d4c3ceb49490da
care bear <carebera@berachain.com>
2024-01-20 20:55:51 -0500

    add util for periodic task

---
+++ task/task_test.go
@@ -0,0 +1,32 @@
+package task
+
+import (
+	"testing"
+	"time"
+)
+
+// TestTask tests the CancellablePeriodicTask function
+// by creating a task that increments a variable every
+// delay_us microseconds, and then cancelling the task
+// after delay_us * 10 microseconds
+// The variable should be incremented 10 times
+func TestTask(t *testing.T) {
+	dummyFunc := func(t *int) {
+		*t += 1
+	}
+
+	v := 0
+	delay_us := uint64(1000)
+	_, cancel := CancellablePeriodicTask(delay_us, &v, dummyFunc)
+
+	// Sleep for delay_us * 10
+	time.Sleep(time.Duration(delay_us*10) * time.Microsecond)
+
+	// Cancel the context
+	cancel()
+
+	// Check the value
+	if v != 10 {
+		t.Errorf("Expected %v, got %v", 10, v)
+	}
+}
